b7cb0cf85799964f480741de9fe8af13ef46f347
looksorn789 <patandjisoo@gmail.com>
2025-05-01 02:59:40 -0700

    created a makeshift header for now and created the landing page, will work on it soon

---
+++ my_daily_journal/src/Pages/LandingPage.js
@@ -0,0 +1,14 @@
+import "../css/LandingPage.css";
+import Header from "../components/Header";
+
+function LandingPage() {
+    return(
+        <div>
+        <Header />
+        <div>
+            <p> HELLO </p>
+        </div>
+        </div>
+    )
+}
+export default LandingPage;
---
+++ my_daily_journal/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../css/Header.css";
+
+const Header = () => {
+
+  return (
+    <header className="header">
+        <p>Test</p>
+    </header>
+  );
+};
+
+export default Header;
